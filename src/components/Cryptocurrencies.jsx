f1cf943ce32f7e4a5f38a60d77526331c7b18dde
Amigors <amigors994@gmail.com>
2021-12-01 19:20:21 +0300

    fix

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
-const Cryptocurrencies = ( {simplified} ) => {
+const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100;
-    const { data: cryptosList, isFetching} = useGetCryptosQuery(count);
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState();
     const [searchTerm, setSearchTerm] = useState()
 
@@ -16,19 +16,21 @@ const Cryptocurrencies = ( {simplified} ) => {
         setCryptos(filteredData)
     }, [cryptosList, searchTerm]);
 
-    if(isFetching) return 'Loading...'
+    if (isFetching) return 'Loading...'
     return (
         <>
-            <div className='search-crypto'>
-                <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
-            </div>
+            {!simplified && (
+                <div className='search-crypto'>
+                    <Input placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
+                </div>
+            )}
             <Row gutter={[32, 32]} className='crypto-card-container'>
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
                         <Link to={`/crypto/${currency.id}`}>
-                            <Card 
+                            <Card
                                 title={`${currency.rank}. ${currency.name}`}
-                                extra={<img className='crypto-image' src={currency.iconUrl}/>}
+                                extra={<img className='crypto-image' src={currency.iconUrl} />}
                                 hoverable
                             >
                                 <p>Price: {millify(currency.price)}</p>
