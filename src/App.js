b5acecb3a15cad43734224daca0799efd6a8bc1f
Amigors <amigors994@gmail.com>
2021-12-01 19:04:11 +0300

    redux toolkit and Cryptocurr

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar, Exchanges, Homepage, CryptoDetails, Cryptocurrencies, News} from './components/index';
+import { Navbar, Exchanges, Homepage, CryptoDetails, Cryptocurrencies, News } from './components/index';
 import './App.css'
 
 const App = () => {
@@ -33,10 +33,17 @@ const App = () => {
                         </Switch>
                     </div>
                 </Layout>
-
-            </div>
-            <div className='footer'>
-
+                <div className='footer'>
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptovrse <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '96b4c460fdmshde5abc5e7c0b238p1a4939jsn8c438b56fa7c'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) =>({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`)
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
